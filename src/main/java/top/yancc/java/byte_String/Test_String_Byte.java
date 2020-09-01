b428d929083fff87a45fcc51d834a7b433d257d5
Ca1m <yancy_01@163.com>
2020-09-01 19:11:29 +0800

    update Map

--- src/main/java/top/yancc/java/byte_String/Test_String_Byte.java
+++ src/main/java/top/yancc/java/byte_String/Test_String_Byte.java
@@ -1,49 +1,63 @@
 package top.yancc.java.byte_String;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
 
-import cn.com.infosec.util.Base64;
+
 
 public class Test_String_Byte {
 
+	private static Encoder encoder = Base64.getEncoder();
+	private static Decoder decoder = Base64.getDecoder();
+	
+	
 	public static void main(String[] args) throws IOException {
 		
-		stringToByte();
-
+		string_To_Byte();
 	}
 	
-	
-	private static void stringToByte() throws IOException {
+	private static void string_To_Byte() throws IOException {
 		
-		String s = "<source><header><TASKTAG>����</TASKTAG><LOCALTIME>1563191937368</LOCALTIME><VERSION>0</VERSION><TYPE>LOGIN</TYPE></header></source>";
+		String s = "A";  // UTF-8
 		
-		byte[] bs = s.getBytes("gbk");
+		byte[] bs_g = s.getBytes("gbk");
+		byte[] bs_g0 = encoder.encode(bs_g);
 		
-		String ss = Base64.encode(bs);
-		
-		
-		byte[] bs_1 = Base64.decode(ss);
-		
-		String ss_1 = new String(bs_1, "UTF-8");
-		String ss_2 = new String(bs_1, "GBK");
+		System.out.println(Arrays.toString(bs_g));  //  中 在 gbk中的编码为 [-42, -48]
+		System.out.println(Arrays.toString(bs_g0)); //    [49, 116, 65, 61]
 		
 		
-		System.out.println(ss);
-		System.out.println(ss_1);
-		System.out.println(ss_2);
+		// 直接输出
+		String s_g1 = new String(bs_g0, "gbk");
+		System.out.println(s_g1);		          // [49, 116, 65, 61]
+		byte[] bs_g1 = s_g1.getBytes("gbk");
+		byte[] bs_g11 = decoder.decode(bs_g1);
+		String s_g11 = new String(bs_g11, "gbk");
+		
+		// 解码，输出
+		byte[] bs_g2 = decoder.decode(bs_g0);
+		String s_g2 = new String(bs_g2, "gbk");
+		System.out.println(s_g2);
 		
 		
-		String s_u = "��";
+		byte[] bs_u = s.getBytes("utf-8");
+		byte[] bs_u0 = encoder.encode(bs_u);
 		
-		byte[] bs_2 = s_u.getBytes("ISO8859-1");
+		// 直接输出
+		String s_u1 = new String(bs_u0, "utf-8");
+		System.out.println(s_u1);
+		byte[] bs_u1 = s_u1.getBytes("utf-8");
+		byte[] bs_u11 = decoder.decode(bs_u1);
+		String s_u11 = new String(bs_u11, "utf-8");
+		System.out.println(s_u11);
 		
-		String ss_u1 = new String(bs_2,"ISO8859-1");
-		
-		System.out.println(ss_u1);
-		
-		
-		
+		// 解码，输出
+		byte[] bs_u2 = decoder.decode(bs_u0);
+		String s_u2 = new String(bs_u2, "utf-8");
+		System.out.println(s_u2);
 		
 		
 	}
---
+++ src/main/java/top/yancc/java/byte_String/Test_read_File.java
@@ -0,0 +1,47 @@
+package top.yancc.java.byte_String;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+   *    测试读取不同的编码的文件并输出
+ * @author Infosec_jy
+ * 
+ * 结论： 读取文件，与编码无关，通过字节序读取文件内容，然后 通过 new String(byte[], charset) 完成转换并显示
+ */
+public class Test_read_File {
+	
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream fis_gbk = new FileInputStream(new File(Test_read_File.class.getClassLoader().getResource("byte_String/read_gbk.txt").getPath()));
+		FileInputStream fis_utf8 = new FileInputStream(new File(Test_read_File.class.getClassLoader().getResource("byte_String/read_utf8.txt").getPath()));
+		
+		byte[] bs_gbk = new byte[fis_gbk.available()];
+		fis_gbk.read(bs_gbk);
+		fis_gbk.close();
+		
+		byte[] bs_utf8 = new byte[fis_utf8.available()];
+		fis_utf8.read(bs_utf8);
+		fis_utf8.close();
+		
+		System.out.println(Arrays.toString(bs_gbk));
+		System.out.println(Arrays.toString(bs_utf8));
+		
+		
+		String str_gbk = new String(bs_gbk, "gbk");      // 正常显示     Base64_read_gbk 测试读取中文问题
+		String str_utf8 = new String(bs_utf8, "utf-8"); // 正常显示 Base64_read_utf8 测试读取中文问题
+		
+		String str_utf8_ = new String(bs_gbk, "utf-8"); // 异常显示 Base64_read_gbk ���Զ�ȡ��������
+		String str_gbk_ = new String(bs_utf8, "gbk");    // 异常显示     Base64_read_utf8 娴嬭瘯璇诲彇涓枃闂
+		
+		System.out.println(str_gbk);
+		System.out.println(str_utf8);
+		
+		System.out.println(str_utf8_);
+		System.out.println(str_gbk_);
+		
+	}
+
+}
