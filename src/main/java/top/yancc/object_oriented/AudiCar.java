324eb72468247bc05989ac6c4b6fb7de8e62be7e
yancy_01 <yancy_01@163.com>
2020-09-13 22:38:07 +0800

    update opject-oriented

---
+++ src/main/java/top/yancc/object_oriented/AudiCar.java
@@ -0,0 +1,16 @@
+package top.yancc.object_oriented;
+
+public class AudiCar extends Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public static void goHome() {
+		
+	}
+	
+	
+}
---
+++ src/main/java/top/yancc/object_oriented/Car.java
@@ -0,0 +1,14 @@
+package top.yancc.object_oriented;
+
+public class Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public static void goHome () {
+		System.out.println("home");
+	}
+}
